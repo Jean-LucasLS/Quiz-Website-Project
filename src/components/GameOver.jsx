89b78af9b8c50c12258b8900c4d68c5a7a869621
Jean-Lucas <jean.lucasls96@gmail.com>
2023-03-29 12:20:32 -0300

    GameOver page stylization

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import WellDone from '../images/welldone.svg';
+
+import './GameOver.css';
 
 const GameOver = () => {
   return (
-    <div>
+    <div id='gameover'>
         <h2>Game Over</h2>
+        <p>Score: x</p>
+        <p>X from Y questions were correct.</p>
+        <img src={WellDone} alt='Quiz ending' />
+        <button>Restart</button>
     </div>
   );
 };
